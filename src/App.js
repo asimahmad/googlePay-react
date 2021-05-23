fff08f29ffeffba2b976b925833481e9f564ae3b
Asim <asim005011997@gmail.com>
2021-05-23 13:49:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import GooglePayButton from '@google-pay/button-react'
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><img src={logo} className="App-logo" alt="img"/>GooglePay React</h1>
+      <hr/>
+      <GooglePayButton 
+      environment="TEST" 
+      paymentRequest={{
+        apiVersion: 2, 
+        apiVersionMinor: 0,
+        allowedPaymentMethods:[
+        {
+          type: 'CARD',
+          parameters: {
+            allowAuthMethods: ['PAN_ONLY', 'CRYTOGRAM_3DS'],
+            allowCardNetworks: ['MASTERCARD', 'VISA']
+          },
+          tokenizationSpecification: {
+            type: 'PAYMENT_GATEWAY',
+            parameters: {
+              gateway: 'example',
+              gatewayMerchantId: 'exampleGatewayMerchantId',
+            },
+          },
+        },
+        ],
+        merchantInfo:{
+          merchantId: '123456789123456789',
+          merchantName: 'Demoer',
+        },
+        transactionInfo: {
+          totalPriceStatus: 'FINAL',
+          totalPriceLabel: 'total',
+          totalPrice: '1',
+          currencyCode: "INR",
+          countryCode: 'in',
+        },
+        shippingAddressRequired:true,
+        callbackIntents: ['PAYMENT_AUTHORIZATION'],
+      }}
+      onLoadPaymentData = {paymentRequest =>{
+        console.log('success', paymentRequest);
+      }}
+      onPaymentAuthorized ={ paymenmentData =>{
+        console.log('Payment is authorized', paymenmentData)
+        return {transactionState: 'SUCCESS'}
+      }}
+      existingPaymentMethodRequired='false'
+      buttonColor= 'black'
+      buttonType='Buy'
+       />
     </div>
   );
 }
