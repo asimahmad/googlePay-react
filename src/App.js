cacbaad45048cc308f2444c56ffac9239a63fd8e
Asim <asim005011997@gmail.com>
2021-05-23 14:34:07 +0530

    Adding gPay into the project with its working state

--- src/App.js
+++ src/App.js
@@ -1,60 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
-import GooglePayButton from '@google-pay/button-react'
-import React from 'react'
+import GooglePayButton from '@google-pay/button-react';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1><img src={logo} className="App-logo" alt="img"/>GooglePay React</h1>
-      <hr/>
-      <GooglePayButton 
-      environment="TEST" 
-      paymentRequest={{
-        apiVersion: 2, 
-        apiVersionMinor: 0,
-        allowedPaymentMethods:[
-        {
-          type: 'CARD',
-          parameters: {
-            allowAuthMethods: ['PAN_ONLY', 'CRYTOGRAM_3DS'],
-            allowCardNetworks: ['MASTERCARD', 'VISA']
-          },
-          tokenizationSpecification: {
-            type: 'PAYMENT_GATEWAY',
-            parameters: {
-              gateway: 'example',
-              gatewayMerchantId: 'exampleGatewayMerchantId',
+      <h1><img src={logo} className="App-logo" alt="logo" /> !!!--- Google Pay React ---!!!</h1>
+      <hr />
+      <GooglePayButton
+        environment="TEST"
+        paymentRequest={{
+          apiVersion: 2,
+          apiVersionMinor: 0,
+          allowedPaymentMethods: [
+            {
+              type: 'CARD',
+              parameters: {
+                allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                allowedCardNetworks: ['MASTERCARD', 'VISA'],
+              },
+              tokenizationSpecification: {
+                type: 'PAYMENT_GATEWAY',
+                parameters: {
+                  gateway: 'example',
+                  gatewayMerchantId: 'exampleGatewayMerchantId',
+                },
+              },
             },
+          ],
+          merchantInfo: {
+            merchantId: '12345678901234567890',
+            merchantName: 'Demo Merchant',
           },
-        },
-        ],
-        merchantInfo:{
-          merchantId: '123456789123456789',
-          merchantName: 'Demoer',
-        },
-        transactionInfo: {
-          totalPriceStatus: 'FINAL',
-          totalPriceLabel: 'total',
-          totalPrice: '1',
-          currencyCode: "INR",
-          countryCode: 'in',
-        },
-        shippingAddressRequired:true,
-        callbackIntents: ['PAYMENT_AUTHORIZATION'],
-      }}
-      onLoadPaymentData = {paymentRequest =>{
-        console.log('success', paymentRequest);
-      }}
-      onPaymentAuthorized ={ paymenmentData =>{
-        console.log('Payment is authorized', paymenmentData)
-        return {transactionState: 'SUCCESS'}
-      }}
-      existingPaymentMethodRequired='false'
-      buttonColor= 'black'
-      buttonType='Buy'
-       />
+          transactionInfo: {
+            totalPriceStatus: 'FINAL',
+            totalPriceLabel: 'Total',
+            totalPrice: '1',
+            currencyCode: 'INR',
+            countryCode: 'IN',
+          },
+          shippingAddressRequired: true,
+          callbackIntents: ['SHIPPING_ADDRESS', 'PAYMENT_AUTHORIZATION'],
+        }}
+        onLoadPaymentData={paymentRequest => {
+          console.log('Success', paymentRequest);
+        }}
+        onPaymentAuthorized={paymentData => {
+            console.log('Payment Authorised Success', paymentData)
+            return { transactionState: 'SUCCESS'}
+          }
+        }
+        onPaymentDataChanged={paymentData => {
+            console.log('On Payment Data Changed', paymentData)
+            return { }
+          }
+        }
+        existingPaymentMethodRequired='false'
+        buttonColor='black'
+        buttonType='Buy'
+      />
     </div>
+    
   );
 }
 
